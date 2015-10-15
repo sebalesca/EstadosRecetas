1b8979a8eca46edbbf3f75d9441eab1d0e7134e7
sebalesca <sebalesca83o@gmail.com>
2015-10-15 17:14:44 -0300

    Refactor saque de node_modules a lib la logica

--- consola.js
+++ consola.js
@@ -1,5 +1,5 @@
 var readline = require('readline');
-var estado = require('verEstados');
+var estado = require('./lib/verEstados');
 var colors = require('colors');
 
 
@@ -10,16 +10,16 @@ var fs = require('fs');
 
 //configuracion de colores
 colors.setTheme({
-  silly: 'rainbow',
-  input: 'grey',
-  verbose: 'cyan',
-  prompt: 'grey',
-  info: 'green',
-  data: 'grey',
-  help: 'cyan',
-  warn: 'yellow',
-  debug: 'blue',
-  error: 'red'
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
 });
 
 
@@ -41,39 +41,37 @@ opciones();
 
 
 rl.on('line', function(line) {
-    
 
+        switch (line.trim()) {
+            case '1':
+                //MUESTRA LOS DATOS DE LA RECETA
+                rl.question("Introduzca el IdReceta a consultar: ", function(idreceta) {
+                    estado.verEstado(idreceta, function(error, Receta) {
+                        console.log('Receta ACES'.yellow);
+                        console.dir(Receta);
+                        console.log('Detalle Receta'.yellow);
+                        //console.log(Receta.imprimir);
+                        opciones();
+                    });
+                    //opciones();  
+                });
 
-    switch (line.trim()) {
-        case '1':
-            //MUESTRA LOS DATOS DE LA RECETA
-            rl.question("Introduzca el IdReceta a consultar: ",function(idreceta){
-              estado.verEstado(idreceta,function(error,Receta){
-               console.log('Receta ACES'.yellow);
-               console.dir(Receta);
-               console.log('Detalle Receta'.yellow);
-               //console.log(Receta.imprimir);
-               opciones();
-              });
-            //opciones();  
-            });
-            
-            break;
-        case '2':
-            rl.question("Introduzca el Codigo de la Farmacia a consultar: ",function(idFarmacia){
-              estado.verEstadoByFarmacia(idFarmacia,function(err,recetas){
-                console.log(recetas);
-                 opciones();
+                break;
+            case '2':
+                rl.question("Introduzca el Codigo de la Farmacia a consultar: ", function(idFarmacia) {
+                        estado.verEstadoByFarmacia(idFarmacia, function(err, recetas) {
+                            console.log(recetas);
+                            opciones();
 
-              });
+                        });
 
-              }
+                    }
 
-          );
-            //console.log('En desarrollo Muchachio....coming soon!');
-           
-            //ACTUALIZA UNA RECETA SEGUN EL IDRECETA
-            /*
+                );
+                //console.log('En desarrollo Muchachio....coming soon!');
+
+                //ACTUALIZA UNA RECETA SEGUN EL IDRECETA
+                /*
             rl.question("Introduzca el id de la receta: ".input, function(idReceta) {
                 actualiza.actualizarReceta(idReceta);
                
@@ -81,63 +79,63 @@ rl.on('line', function(line) {
                 opciones();
 
             });*/
-            break;
-        case '3':
-             console.log('En desarrollo Muchachio....coming soon!');
-             opciones();
-        /*
-            console.log('Se Actualizaran las Recetas Pendientes!');
-              
+                break;
+            case '3':
+                //console.log('En desarrollo Muchachio....coming soon!');
+                //opciones();
+
+                console.log('Se Actualizaran las Recetas Pendientes!');
+
                 //var cant_sinprocesar;
                 //var cant_procesadas;
-                        
-            actRecetas.getPendientes(function(error,Pendientes){
-                              
-               if (!error) {
-                    //chequeo por cada una el estado en el web service de aces
-                    console.log('encontro las pendientes');
-                    _.each(Pendientes, function(recetaFLV, n) {
-                             actRecetas.verEstadoAces(recetaFLV, function(err, recetaAces) {
-                                  if (!err){
 
-                                                 if (recetaAces.Table.RegID == recetaFLV.regid) {
-                                                    contar_sinprocesar();
-                                                  // console.log('**********************ESTADOS**************************************');
-                                                   // console.log('estado igual no se procesa');
-                                                  //  console.log('CODIGO FLV:' + recetaFLV.regid);
-                                                  //  console.log('CODIGO ACES:' + recetaAces.Table.RegID);
-                                                  //  console.log('**********************CAMPOS DE LA RECETA ACES*******************************');
-                                                  //  console.log('***********************FIN RECETA ACES***********************');
-                                                } else {
-                                                    //SI CAMBIO EL REG ID DEBO PROCESAR LA RECETA Y ACTUALIZAR EL ESTADO EN FLV
-                                                    contar_Procesadas();
-                                                    console.log('El Estado CAMBIO se procesara');
-                                                    console.log('CODIGO FLV:' + recetaFLV.regid);
-                                                    console.log('CODIGO ACES:' + recetaAces.Table.RegID);
+                actRecetas.getPendientes(function(error, Pendientes) {
 
-                                                    actRecetas.procesaEstado(recetaAces, function(err) {
-                                                        if (!err) {
+                    if (!error) {
+                        //chequeo por cada una el estado en el web service de aces
+                        console.log('encontro las pendientes');
+                        _.each(Pendientes, function(recetaFLV, n) {
+                            actRecetas.verEstadoAces(recetaFLV, function(err, recetaAces) {
+                                if (!err) {
 
-                                                            console.log('se proceso OK RECETA:' + recetaAces.Table.idRecetaFLV);
-                                                        } else {
-                                                            console.log('algo ha fallado RECETA: ' + recetaAces.Table.idRecetaFLV);
-                                                        }
+                                    if (recetaAces.Table.RegID == recetaFLV.regid) {
+                                        contar_sinprocesar();
+                                        // console.log('**********************ESTADOS**************************************');
+                                        // console.log('estado igual no se procesa');
+                                        //  console.log('CODIGO FLV:' + recetaFLV.regid);
+                                        //  console.log('CODIGO ACES:' + recetaAces.Table.RegID);
+                                        //  console.log('**********************CAMPOS DE LA RECETA ACES*******************************');
+                                        //  console.log('***********************FIN RECETA ACES***********************');
+                                    } else {
+                                        //SI CAMBIO EL REG ID DEBO PROCESAR LA RECETA Y ACTUALIZAR EL ESTADO EN FLV
+                                        contar_Procesadas();
+                                        console.log('El Estado CAMBIO se procesara');
+                                        console.log('CODIGO FLV:' + recetaFLV.regid);
+                                        console.log('CODIGO ACES:' + recetaAces.Table.RegID);
 
-                                                    });//fin procesa estado
+                                        actRecetas.procesaEstado(recetaAces, function(err) {
+                                            if (!err) {
 
-                                                }
-                                            }//fin err verEstados
-                                                             
-                             });//fin ver estados
+                                                console.log('se proceso OK RECETA:' + recetaAces.Table.idRecetaFLV);
+                                            } else {
+                                                console.log('algo ha fallado RECETA: ' + recetaAces.Table.idRecetaFLV);
+                                            }
+
+                                        }); //fin procesa estado
+
+                                    }
+                                } //fin err verEstados
+
+                            }); //fin ver estados
                         });
-                         fs.appendFileSync('logRecetas.txt', 'Cantidad de pendientes que se procesaron:  y estaban igual:   \r');//, function(err) {
-                                        /*if (err) {
+                        fs.appendFileSync('logRecetas.txt', 'Cantidad de pendientes que se procesaron:  y estaban igual:   \r'); //, function(err) {
+                        /*if (err) {
                                             console.log(err);
                                         } else {
                                             console.log('Se ha escrito correctamente el log');
                                         }
-                                    });*///fin del each
-              /*  }else{
+                                    });*/ //fin del each
+                          }else{
                         console.log('Error al tratar de actualizar Pendientes');
                 }
                    // opciones();
@@ -145,7 +143,7 @@ rl.on('line', function(line) {
             });//fin getPendientes
         
 
-            */
+            
             break;
 
         case '4':
@@ -204,11 +202,11 @@ rl.on('line', function(line) {
                 opciones();
             });//fin getPendientes
             */
-            break;
-        case '5':
-             console.log('En desarrollo Muchachio....coming soon!');
-             opciones();
-            /*
+                        break;
+                        case '5':
+                            console.log('En desarrollo Muchachio....coming soon!');
+                            opciones();
+                            /*
               console.log('Se Actualizaran las Recetas Observadas!');
             actRecetas.getObservadas(function(error,Observadas){
                if (!error) {
@@ -249,22 +247,22 @@ rl.on('line', function(line) {
                   opciones();
             });//fin getobservadas
           */
-            break;
-        case '6':
-            
-              rl.close();
-            break;
-        
-        default:
-            console.log('Este no es un parametro valido verifica las opciones disponibles');
-            //muestro las opcione nuevamente
-            opciones();
-            break;
+                            break;
+                        case '6':
 
-    }
+                            rl.close();
+                            break;
 
-   // rl.prompt();
-}).on('close', function() {
-    console.log('Hasta luego MUCHACHO!!!');
-    process.exit(0);
-});
+                        default:
+                            console.log('Este no es un parametro valido verifica las opciones disponibles');
+                            //muestro las opcione nuevamente
+                            opciones();
+                            break;
+
+                    }
+
+                    // rl.prompt();
+                }).on('close', function() {
+                    console.log('Hasta luego MUCHACHO!!!');
+                    process.exit(0);
+                });
